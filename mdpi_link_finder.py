522b547b91b0d09d1d13d3d224ef6909a29cb0f0
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2021-01-02 11:45:27 -0500

    gathers links for metadata scraping for MPDI papers

--- mdpi_link_finder.py
+++ mdpi_link_finder.py
@@ -5,29 +5,29 @@
 import urllib
 from bs4 import BeautifulSoup as bs
 
-# Defining results pages from an empty search query (there are 11127 pages)
+# Defining results pages from an empty search query (there are 18326 pages)
 # The selected options include page count (15 results per page), year from (2019), and year to (2020)
 # Do not increase the results per page or you will not get data!
 
 start = 'https://www.mdpi.com/search?sort=pubdate&page_no='
-end = '&page_count=15&year_from=1996&year_to=2020&view=default'
+end = '&page_count=15&year_from=2019&year_to=2020&view=default'
 
 # Scraping the metadata from all submissions
 
 links = []
 
-for i in range(24378):
+for i in range(18326):
    
     # Declaring which results page from the empty search query we want to scrape
    
     url = start + str(i+1) + end
-    print('Retrieving data from query page ' + str(i+1) + ' of 24378.')
+    print('Retrieving data from query page ' + str(i+1) + ' of 18326.')
    
     # Getting the raw data
     
-    if i == 24378:
+    if i == 18326:
         
-        length = len(links) + 8
+        length = len(links) + 4
         
     else:
         
@@ -61,8 +61,12 @@ for i in range(24378):
         except:
            
             continue
+        
+# Get rid of duplicate links
+
+links = list(set(links))
        
-with open('C:/Users/themadcatter/Documents/Data/COVID-19/mdpi_links.txt', 'w') as file:
+with open('C:/Users/User/Documents/Data/COVID-19/mdpi_links.txt', 'w') as file:
    
     for link in links:
        
